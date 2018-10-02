1085a7efbb1456fe9d35c0bcb8ffb24e96d7f8f3
YuPengLu <ylu1100@bths.edu>
2018-10-02 08:38:41 -0400

    isPalindrome and dateStr

--- src/YuPengLu/Main.java
+++ src/YuPengLu/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("isPalindrome(racecar) = " + (YuPengLuLib.isPalindrome("racecar")));
+        System.out.println("dateStr(01/08/1999) = " + (YuPengLuLib.dateStr("01/08/1999")));
     }
 }
--- src/YuPengLu/YuPengLuLib.java
+++ src/YuPengLu/YuPengLuLib.java
@@ -1,4 +1,19 @@
 package YuPengLu;
 
 public class YuPengLuLib {
+    public static boolean isPalindrome(String input){
+        String output = "";
+                for(int i = input.length();i>0;i--) {
+                    output = output + input.substring(i-1,i);
+                }
+                return (input.equals(output));
+    }
+    public static String dateStr(String input) {
+        String output = "";
+        String mm = input.substring(0, 2);
+        String dd = input.substring(3, 5);
+        String yy = input.substring(6);
+        output = output + dd + " - " + mm + " - " + yy;
+        return output;
+    }
 }
