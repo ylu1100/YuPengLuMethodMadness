01d0c1fdcaea680628989eac97fdb8587391cfcc
yupeng <ylu1100@bths.edu>
2018-10-04 20:20:02 -0400

    done

--- src/YuPengLu/Main.java
+++ src/YuPengLu/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("dateStr(01/08/1999) = " + (YuPengLuLib.dateStr("01/08/1999")));
         System.out.println("sumUpTo(27) = " + (YuPengLuLib.sumUpTo(27)));
         System.out.println("isFibonnaci(34)= " + (YuPengLuLib.isFibonacci(34)));
+        System.out.println("cutOut(catatonic cat,cat) = " + (YuPengLuLib.cutOut("catatonic cat","cat")));
     }
 }
